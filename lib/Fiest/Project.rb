ec7b168942408b69f0c1dc8f42aeb038aa7b6a8f
nichervan brhik <nichervan.brhik@gmail.com>
2017-01-31 12:12:17 +0000

    added my changes

---
+++ lib/Fiest/Project.rb
@@ -0,0 +1,7 @@
+require "Fiest/Project/version"
+
+module Fiest
+  module Project
+    # Your code goes here...
+  end
+end
---
+++ lib/Fiest/Project/version.rb
@@ -0,0 +1,5 @@
+module Fiest
+  module Project
+    VERSION = "0.1.0"
+  end
+end
